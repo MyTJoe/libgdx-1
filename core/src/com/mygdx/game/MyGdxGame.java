d2f5b92297d4c1906826639c869c681923d82db8
Joe Langley <josephelangley@gmail.com>
2017-01-08 18:12:35 -0500

    got the batches done

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,29 +5,81 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private TextureRegion down, up, right, left;
+	private int facing;
+	private float time;
+
+	private float x, y, yv, xv;
+	private static float MAX_VELOCITY = 100;
+
+	public static final int UP = 1;
+	public static final int DOWN = 2;
+	public static final int LEFT = 3;
+	public static final int RIGHT = 4;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Texture tiles = new Texture("tiles.png");
+		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
+		down = grid[6][0];
+		up = grid[6][1];
+		right = grid[6][3];
+		left = new TextureRegion(right);
+		left.flip(true, false);
 	}
 
 	@Override
 	public void render () {
+		time += Gdx.graphics.getDeltaTime();
+		move();
+		TextureRegion img = down;
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
+
+		if(facing == UP) {
+			batch.draw(up, x, y);
+		} else if (facing == DOWN) {
+			batch.draw(down, x, y);
+		} else if (facing == RIGHT) {
+			batch.draw(right, x, y);
+		} else if (facing == LEFT) {
+			batch.draw(left, x, y);
+		}
 		batch.end();
 	}
-	
+
+	float decelerate(float velocity) {
+		float deceleration = 0.95f; // the closer to 1, the slower the deceleration
+		velocity *= deceleration;
+		if (Math.abs(velocity) < 1) {
+			velocity = 0;
+		}
+		return velocity;
+	}
+
+	public void move() {
+		if(Gdx.input.isKeyPressed(UP)) {
+		yv = MAX_VELOCITY;
+		facing = UP;
+		if(Gdx.input.isKeyPressed(SPACE))
+	}
+		y += yv * Gdx.graphics.getDeltaTime();
+		x += xv * Gdx.graphics.getDeltaTime();
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
