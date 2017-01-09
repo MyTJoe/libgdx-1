be9bdfe2e716657f43a6b1096156bdb732cce3ab
Joe Langley <josephelangley@gmail.com>
2017-01-08 22:47:58 -0500

    think I made this too complicated - can't figure out how to handle the walk option with how I set everything up

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,58 +4,95 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private TextureRegion down, up, right, left, stand;
+	TextureRegion tree;
 	private int facing;
 	private float time;
-
-	private float x, y, yv, xv = 0;
+	private float x, y, yv, xv;
 	private static float MAX_VELOCITY = 100;
 
-	public static final int UP = 1;
-	public static final int DOWN = 2;
-	public static final int LEFT = 3;
-	public static final int RIGHT = 4;
+	private static final int UP = 1;
+	private static final int DOWN = 2;
+	private static final int LEFT = 3;
+	private static final int RIGHT = 4;
+	private static final int STAND = 5;
+
+	private static final int WIDTH = 16;
+	private static final int HEIGHT = 16;
+
+	private static final int DRAW_WIDTH = WIDTH * 3;
+	private static final int DRAW_HEIGHT = HEIGHT * 3;
+
+	private OrthographicCamera camera;
+	private TiledMapRenderer tiledMapRenderer;
+
 
 	@Override
 	public void create () {
+		TiledMap tiledMap;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("level1.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
 		batch = new SpriteBatch();
 		Texture tiles = new Texture("tiles.png");
-		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
+		tree = new TextureRegion(tiles,0, 8, 16, 16);
+		TextureRegion[][] grid = TextureRegion.split(tiles, WIDTH, HEIGHT);
 		stand = grid[6][2];
 		down = grid[6][0];
 		up = grid[6][1];
 		right = grid[6][3];
 		left = new TextureRegion(right);
 		left.flip(true, false);
+
 	}
 
 	@Override
 	public void render () {
+		time += Gdx.graphics.getDeltaTime();
 		move();
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 
 		TextureRegion img = up;
-		time += Gdx.graphics.getDeltaTime();
 		batch.begin();
-		batch.draw(img, x, y);
+		batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 0,100, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 232,50, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 300,167, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 500,425, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 23,399, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 555,19, DRAW_WIDTH, DRAW_HEIGHT);
+		batch.draw(tree, 400, 375, DRAW_WIDTH, DRAW_HEIGHT);
 
-		if(facing == UP) {
-			batch.draw(up, x, y);
-		} else if (facing == DOWN) {
-			batch.draw(down, x, y);
+		if (facing == UP) {
+			batch.draw(up, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		} else if (facing == RIGHT) {
-			batch.draw(right, x, y);
+			batch.draw(right, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		} else if (facing == LEFT) {
-			batch.draw(left, x, y);
+			batch.draw(left, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		} else if (facing == DOWN) {
+			batch.draw(down, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		} else if (facing == STAND) {
+			batch.draw(stand, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		}
 		batch.end();
 	}
@@ -70,32 +107,43 @@ public class MyGdxGame extends ApplicationAdapter {
 	}
 
 	public void move() {
+
+		if (y < 0) {
+			y = Gdx.graphics.getHeight();
+		} else if (y > Gdx.graphics.getHeight()-1) {
+			y = 0;
+		}
+		if (x < 0) {
+			x = Gdx.graphics.getWidth();
+		} else if (x > Gdx.graphics.getWidth()-1) {
+			x = 0;
+		}
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
 			yv = MAX_VELOCITY;
 			facing = UP;
 			if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-				yv *= 3;
+				yv += MAX_VELOCITY;
 			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			yv = MAX_VELOCITY * -1; // yv = MAX_VELOCITY * -1;
+			yv = MAX_VELOCITY * -1;
 			facing = DOWN;
 			if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-				yv *= MAX_VELOCITY * -3;
+				yv -= MAX_VELOCITY;
 			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 			xv = MAX_VELOCITY;
 			facing = RIGHT;
 			if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-				yv *= MAX_VELOCITY * 3;
+				xv += MAX_VELOCITY;
 			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			xv = MAX_VELOCITY * -1;
 			facing = LEFT;
 			if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-				yv *= MAX_VELOCITY * -3;
+				xv -= MAX_VELOCITY;
 			}
 		}
 
@@ -109,5 +157,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
+
 	}
 }
